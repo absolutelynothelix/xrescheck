211478fdcc51d50dc32369c002c833709b8d0a11
Maxim Solovyov <msolovyov@protonmail.com>
2023-02-13 14:32:51 +0300

    fix building on some systems

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh -ex
 
 mkdir -p build
-gcc -shared -fpic -DXRC_INTERCEPT_XCB_PIXMAPS -obuild/xrescheck_xcb_pixmaps.so \
+
+CC="gcc"
+CFLAGS="-shared -fpic -D_GNU_SOURCE"
+
+$CC $CFLAGS -DXRC_INTERCEPT_XCB_PIXMAPS -obuild/xrescheck_xcb_pixmaps.so \
 src/xrescheck.c
-gcc -shared -fpic -DXRC_INTERCEPT_XCB_GCS -obuild/xrescheck_xcb_gcs.so \
-src/xrescheck.c
-gcc -shared -fpic -DXRC_INTERCEPT_XCB_COMPOSITE_NAMED_WINDOWS_PIXMAPS \
+$CC $CFLAGS -DXRC_INTERCEPT_XCB_GCS -obuild/xrescheck_xcb_gcs.so src/xrescheck.c
+$CC $CFLAGS -DXRC_INTERCEPT_XCB_COMPOSITE_NAMED_WINDOWS_PIXMAPS \
 -obuild/xrescheck_xcb_composite_named_windows_pixmaps.so src/xrescheck.c
-gcc -shared -fpic -DXRC_INTERCEPT_XCB_RENDER_PICTURES \
+$CC $CFLAGS -DXRC_INTERCEPT_XCB_RENDER_PICTURES \
 -obuild/xrescheck_xcb_render_pictures.so src/xrescheck.c
